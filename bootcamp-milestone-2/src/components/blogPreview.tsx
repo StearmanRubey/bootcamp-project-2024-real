967c9f66fe71a805a5759831a71f1a42d6c2024e
StearmanRubey <rubey@calpoly.edu>
2024-11-07 20:33:34 -0800

    feat: added working link with slug

--- bootcamp-milestone-2/src/components/blogPreview.tsx
+++ bootcamp-milestone-2/src/components/blogPreview.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import style from './blogPreview.module.css';
 import { Blog } from "@/app/blogData";
 import Image from 'next/image';
+import Link from "next/link";
 
 export default function BlogPreview(props: Blog) {
   return (
-		// replace everything between the <div> & </div> tags
-		// with your code from earlier milestones
     <div className={style.div}>
       <h3>{props.title}</h3>
-      <div>
+      <h3>{props.date}</h3>
+      <Link href={`blog/${props.slug}`} >Learn More!</Link>
+      <div className={style.inner}>
         <Image src = {props.image} alt = {props.imageAlt} width={700} height={394} ></Image>
         <p>{props.description}</p>
-        <p>{props.date}</p>
       </div>
 	</div>
   );
