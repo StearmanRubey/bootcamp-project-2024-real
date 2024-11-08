0bd6c8566fc341bd1a54fefb7106c18c90c581ae
StearmanRubey <rubey@calpoly.edu>
2024-11-07 20:34:23 -0800

    fix: removed extra link

--- bootcamp-milestone-2/src/components/navbar.tsx
+++ bootcamp-milestone-2/src/components/navbar.tsx
@@ -8,10 +8,9 @@ export default function Navbar() {
       <h1> Stearman's Personal Website </h1>
       <nav className={style.navText}>
         <Link href="/" >Home</Link>
-        <Link href="/blogs">Blog</Link>
         <Link href="/portfolio">Portfolio</Link>
         <Link href="/resume" >Resume</Link>
-        <Link href="/about" >Contact</Link>
+        <Link href="/contact" >Contact</Link>
       </nav>
     </header>
   );
