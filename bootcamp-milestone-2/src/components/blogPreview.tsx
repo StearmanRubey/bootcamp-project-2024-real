888abb7cf7bdf56e61dbc51894bf532d9219c7a8
StearmanRubey <rubey@calpoly.edu>
2024-11-16 18:20:28 -0800

    fix: changed blogPreview to accomodate database

--- bootcamp-milestone-2/src/components/blogPreview.tsx
+++ bootcamp-milestone-2/src/components/blogPreview.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import style from './blogPreview.module.css';
-import { Blog } from "@/app/blogData";
+import Blog from "@/database/blogSchema";
 import Image from 'next/image';
 import Link from "next/link";
 
 export default function BlogPreview(props: Blog) {
+
+  const renderImage = typeof props.image === "string" && props.image !== "";
+
+
   return (
     <div className={style.div}>
       <h3>{props.title}</h3>
-      <h3>{props.date}</h3>
+      <h3>{props.date.toLocaleDateString()}</h3>
       <Link href={`blog/${props.slug}`} >Learn More!</Link>
       <div className={style.inner}>
-        <Image src = {props.image} alt = {props.imageAlt} width={700} height={394} ></Image>
+        { renderImage && (
+          <Image src = {props.image} alt = {props.imageAlt} width={700} height={394} ></Image>
+          )
+        }
         <p>{props.description}</p>
       </div>
 	</div>
